3c7e2aabdca24f55a3357723b2602ff55094c55e
psmelero <psmelero@hiberus.com>
2018-05-07 23:22:41 +0200

    [edd] added titles

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,7 +24,7 @@
                         <v-icon v-html="item.icon"></v-icon>
                     </v-list-tile-action>
                     <v-list-tile-content>
-                        <v-list-tile-title v-text="item.title"></v-list-tile-title>
+                        <v-list-tile-title v-text="item.title" :title="item.title"></v-list-tile-title>
                     </v-list-tile-content>
                 </v-list-tile>
             </v-list>
