9cf321e354c33b8d0e744dfa002095c416c39438
Paul Melero <paul.melero@erni-espana.es>
2018-07-18 15:48:24 +0200

    [tests] edited tests encapsulation proccess and added babel plugin

--- test/unit/specs/pages/1-hello-world.spec.js
+++ test/unit/specs/pages/1-hello-world.spec.js
@@ -4,7 +4,7 @@ import {
 } from '@vue/test-utils';
 import Vuex from 'vuex';
 import Vuetify from 'vuetify';
-import Blog from '~/pages/exercise-2-blog/index.vue';
+import Hello from '~/pages/exercise-1-hello-world/index.vue';
 import store from '../../../fixtures/store';
 
 describe('Home /', () => {
@@ -16,13 +16,17 @@ describe('Home /', () => {
         localVue.use(Vuex);
     });
 
-    it('renders Blog page', () => {
-        const wrapper = mount(Blog, {
+    it('renders Hello world page', () => {
+        const wrapper = mount(Hello, {
             localVue: localVue,
             store: store(),
-            stubs: ['nuxt-link']
+            stubs: ['nuxt-link', 'the-satelite']
         });
         expect(wrapper.isVueInstance()).toBeTruthy();
         expect(wrapper.vm.$el).toMatchSnapshot();
+    });
+
+    afterAll(() => {
+        localVue.destroy();
     });
 });
